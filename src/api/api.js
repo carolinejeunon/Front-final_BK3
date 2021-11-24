13d079f80a1e69ac44e34bda497657ceab0877fa
pfreisleben <pedrofreislebeen@gmail.com>
2021-11-23 21:41:08 -0300

    ok

--- src/api/api.js
+++ src/api/api.js
@@ -1,30 +1,30 @@
 const Api = {
   apiUrl: 'https://projeto03-blue.herokuapp.com/acao',
-  fetchGetAll: () => fetch(Api.apiUrl),
-  fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
+  fetchGetAll: () => fetch(`${Api.apiUrl}/listall`),
+  fetchGetSingle: (id) => fetch(`${Api.apiUrl}/listid/${id}`),
   fetchPost: (acao) => {
-    return fetch(`${Api.apiUrl}/add`, {
+    return fetch(`${Api.apiUrl}/create`, {
       method: 'POST',
       headers: new Headers({
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       }),
-      body: JSON.stringify(acao)
-    })
+      body: JSON.stringify(acao),
+    });
   },
   fetchPut: (acao, id) => {
     return fetch(`${Api.apiUrl}/update/${id}`, {
       method: 'PUT',
       headers: new Headers({
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       }),
-      body: JSON.stringify(acao)
-    })
+      body: JSON.stringify(acao),
+    });
   },
   fetchDelete: (id) => {
     return fetch(`${Api.apiUrl}/delete/${id}`, {
-      method: 'DELETE'
-    })
-  }
-}
+      method: 'DELETE',
+    });
+  },
+};
 
 export default Api;
